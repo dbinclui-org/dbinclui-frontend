ce18e221ce627d88cb60a2a34c469669def3cc9f
Ketryn <ketryn29@gmail.com>
2021-12-23 10:41:09 -0300

    configuracao Prettier

--- src/pages/home/home.test.tsx
+++ src/pages/home/home.test.tsx
@@ -9,11 +9,11 @@ describe('Componente do Home', () => {
     const pageBoxOne = 'TRADUTOR DE LIBRAS';
     const pageBoxTwo = 'GUIA DE ACESSIBILIDADE';
     const pageBoxThree = 'GUIA DA CULTURA SURDA';
-    
+
     const pageBoxCardOne = screen.getByText(pageBoxOne);
-    const pageBoxCardTwo= screen.getByText(pageBoxTwo);
+    const pageBoxCardTwo = screen.getByText(pageBoxTwo);
     const pageBoxCardThree = screen.getByText(pageBoxThree);
-    
+
     expect(pageBoxCardOne.textContent).toBe(pageBoxOne);
     expect(pageBoxCardTwo.textContent).toBe(pageBoxTwo);
     expect(pageBoxCardThree.textContent).toBe(pageBoxThree);
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,67 +4,76 @@ import './style.css';
 import { Row } from 'react-bootstrap';
 
 export default function Home() {
-     
   return (
     <Row>
       <main className="conteudo" role="main">
         <section className="container-home">
-          <input 
-            type="text" 
-            className="box-busca" 
-            placeholder="pesquise aqui" 
-            role="search"/>
-          <p>Bem-vindo ao DBINCLUI, lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Etiam mattis fringilla dolor, id congue diam rhoncus sit amet. Fusce at lacus metus. 
-            Maecenas gravida  finibus ligula, vitae lacinia est. Integer tristique libero non nunc 
-            faucibus elementum. 
+          <input
+            type="text"
+            className="box-busca"
+            placeholder="pesquise aqui"
+            role="search"
+          />
+          <p>
+            Bem-vindo ao DBINCLUI, lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Etiam mattis fringilla dolor, id congue diam
+            rhoncus sit amet. Fusce at lacus metus. Maecenas gravida finibus
+            ligula, vitae lacinia est. Integer tristique libero non nunc
+            faucibus elementum.
           </p>
         </section>
       </main>
-          
+
       <section className="container-center">
-        <div 
-          className="box"  
-          role="button" 
-          tabIndex={1} 
-          aria-label="TRADUTOR DE LIBRAS">
-            <h3 className="title" id="card1">TRADUTOR DE LIBRAS</h3>
+        <div
+          className="box"
+          role="button"
+          tabIndex={1}
+          aria-label="TRADUTOR DE LIBRAS"
+        >
+          <h3 className="title" id="card1">
+            TRADUTOR DE LIBRAS
+          </h3>
 
           <div className="box-libras">
             <Link to="">
-              <img src="" alt=""/>
-            </Link>        
-          </div>  
-        </div>
-
-        <div 
-          className="box"  
-          role="button" 
-          tabIndex={2} 
-          aria-label="GUIA DE ACESSIBILIDADE">
-            <h3 className="title" id="card2" >GUIA DE ACESSIBILIDADE</h3>
-          <div className="box-libras">
-            <Link to="">
-              <img src="" alt=""/>
+              <img src="" alt="" />
             </Link>
           </div>
         </div>
 
-        <div 
-          className="box" 
-          role="button" 
-          tabIndex={3} 
-          aria-label="GUIA DA CULTURA SURDA">
-            <h3 className="title" id="card3" >GUIA DA CULTURA SURDA</h3>
+        <div
+          className="box"
+          role="button"
+          tabIndex={2}
+          aria-label="GUIA DE ACESSIBILIDADE"
+        >
+          <h3 className="title" id="card2">
+            GUIA DE ACESSIBILIDADE
+          </h3>
           <div className="box-libras">
             <Link to="">
-              <img src="" alt=""/>
+              <img src="" alt="" />
+            </Link>
+          </div>
+        </div>
+
+        <div
+          className="box"
+          role="button"
+          tabIndex={3}
+          aria-label="GUIA DA CULTURA SURDA"
+        >
+          <h3 className="title" id="card3">
+            GUIA DA CULTURA SURDA
+          </h3>
+          <div className="box-libras">
+            <Link to="">
+              <img src="" alt="" />
             </Link>
           </div>
         </div>
       </section>
-
     </Row>
   );
-
 }
